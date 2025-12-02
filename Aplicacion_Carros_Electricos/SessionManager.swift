b9afb4e22c618395340f03bf2a7857de1b10472a
donlitle <cesar.reyes.cortinas12@gmail.com>
2025-12-01 20:27:26 -0600

    ya muestra los datos del feed que es el carro que esta.

---
+++ Aplicacion_Carros_Electricos/SessionManager.swift
@@ -0,0 +1,8 @@
+//
+//  SessionManager.swift
+//  Aplicacion_Carros_Electricos
+//
+//  Created by donlike on 01/12/25.
+//
+
+import Foundation
---
+++ Aplicacion_Carros_Electricos/UserSession.swift
@@ -0,0 +1,8 @@
+//
+//  UserSession.swift
+//  Aplicacion_Carros_Electricos
+//
+//  Created by donlike on 01/12/25.
+//
+
+import Foundation
